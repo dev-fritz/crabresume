eb72d8e9c8cb1b5fbfba31fe8bf4984740cad1e3
Fritz Henrique <fritzhenrique.dev@gmail.com>
2024-11-01 21:02:54 -0400

    feat: added code base to create server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+use crabresume::run;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let listener = TcpListener::bind("0.0.0.0:8000").expect("Failed on bind port.");
+    run(listener)?.await
 }
